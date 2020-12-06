b0997814dbf0f5234c003a32c34e315db2534858
Fwedwic <48993381+Fwedwic@users.noreply.github.com>
2020-12-06 10:34:47 +0100

    correction du python

--- D2-Passwd_Policy/d2-pwd_Policy.py
+++ D2-Passwd_Policy/d2-pwd_Policy.py
@@ -8,32 +8,19 @@
 
 
 
-compteur_Faux = 0
+compteur_vrai = 0
 
 with open("input","r") as f:
-	my_Array = f.readlines()
+	my_array = f.readlines()
 	
-for i in range(len(my_Array)):
-	mini = int(my_Array[i].split('-')[0])
-	maxi = int(my_Array[i].split('-')[1].split(' ')[0])
-	token = my_Array[i].split(' ')[1][0]
-	password = my_Array[i].split(':')[1]
+for line in my_array:
+	mini = int(line.split('-')[0])
+	maxi = int(line.split('-')[1].split(' ')[0])
+	token = line.split(' ')[1][0]
+	password = line.split(':')[1]
 	
-	liste_Token = []
-	liste_Token = [ letter for letter in password if letter == token ]
+	if (password.count(token) >= mini and password.count(token) <= maxi):
+		compteur_vrai +=1
 	
-	if len(liste_Token) < mini:
-		compteur_Faux += 1
-	elif len(liste_Token) > maxi:
-		compteur_Faux +=1
-	
-	#print(liste_Token)
-	#print(token)
-	#print(len(liste_Token))
-	#print(mini)
-	#print(maxi)
-	#print(compteur_Faux)
-	#input("?")
-		
-print(compteur_Faux)
+print(compteur_vrai)
 
